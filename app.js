9ebf044c8c24c7e2ad525ee68cc0b96d28534121
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2025-07-12 11:23:46 +0530

    js apna college

---
+++ app.js
@@ -0,0 +1,78 @@
+//console.log("Helloworld");
+//console.log("YO Yo")
+//a=10;
+//b=1;
+//console.log("sum is:",a+b);
+//let pencilPrice=10;
+//let eraserPrice=5;
+//console.log("The total price is:",pencilPrice+eraserPrice,"rupees");
+//console.log(`Your Bill amount is ${pencilPrice+eraserPrice} Rupees.`)
+/*console.log("Before if statement");
+let marks=75;
+if(!(marks<33)){
+    console.log("pass");
+    console.log("a++");*/
+
+/*let str="apple";
+if(str[0]=="a" && str.length>3){
+    console.log("goodstring");
+}
+let num=12;
+if ((num%3===0)&&((num+1==15)||(num-1==11))) {
+    console.log("safe");
+}else{
+    console.log("unsafe");
+}
+console.log
+("this is asimple log");
+console.error("this is an error");
+console.warn("this is a warn");*/
+// let firstName=prompt("enter your first name");
+// let lastName=prompt("enter your last name");
+// console.log("Welcome",firstName,"",lastName,"!");
+// let msg="    Aloo   ";
+// let n=5;
+// let m=10;
+// for (let i = 1; i<=m; i++) {
+//     console.log(`${n} x ${i} = ${n * i}`)
+// }
+// for( let i=10;i>=1;i--){
+//     console.log(i)
+// }
+
+// for(let i =0;i<=10;i+=2){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=3;i++){
+//     console.log(`outer loop`)
+//     for(let j=1;j<=3;j++){
+//         console.log(j)
+//     }
+// }
+// let i=5;
+// while (i>=1){
+//     console.log(i)
+//     i--;
+// }
+
+// let i=1;
+// while(i<=10){
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+// console.log("we use break at 3")
+// let fruits=["mango","apple","banana","litchi","orange","guava"];
+// fruits.push("Dates")
+// fruits.unshift("Tomato")
+
+// for (let i=0;i<fruits.length;i++){
+//     console.log(i,fruits[i]);
+// }
+// for (let i=fruits.length-1;i>=0;i--){
+//     console.log(i,fruits[i])
+// }
+let heroes=[]
